fa1bbcff32813e41be17a8747df5a1967775c757
Mykolai Skydan <117273969+ewqwdd@users.noreply.github.com>
2025-03-10 09:35:11 +0200

    megakompyuter init

--- routes/depositRouter.js
+++ routes/depositRouter.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const authMiddleware = require("../middleware/authMiddleware");
 const { Transaction, User } = require("../models");
-const { createDeposit, createDepositKhati, createDepositEsewa } = require("../lib/paymentService");
+const { createDeposit } = require("../lib/paymentService");
 const { default: axios } = require("axios");
 const { generateOrderId } = require("../lib/generateOrderId");
-const { AxiosError } = require("axios");
 require("dotenv").config();
 
 const router = express.Router();
 
-router.post("/khati", authMiddleware, async (req, res) => {
+router.post("/", authMiddleware, async (req, res) => {
   try {
     const { amount } = req.body;
     const id = req.user.id;
@@ -21,107 +20,68 @@ router.post("/khati", authMiddleware, async (req, res) => {
       status: "pending",
       orderId,
     });
-    const user = await User.findByPk(id)
-    const deposit = await createDepositKhati(orderId, amount * 1.01, 'Deposit',user);
 
-    if (deposit?.payment_url) {
-      return res.json({ url: deposit.payment_url });
-    }
+    const payUrl = process.env.CLICK_API + `/services/pay?service_id=${process.env.CLICK_SERVICE_ID}` +
+    `&merchant_id=${process.env.CLICK_MERCHANT_ID}` +
+    `&amount=${amount}` + 
+    `&transaction_param=${orderId}` +
+    `&return_url=${process.env.BASE_URL + '/notify'}`
 
-    return res.status(400).json({ success: false });
+    return res.json({
+      url: payUrl
+    })
   } catch (error) {
-    if (error instanceof AxiosError) {
-      console.error(error.response.data)
-    } else {
-      console.error("Unable to connect to the database:", error);
-    }
-    res.status(500).json({ message: "Server error" });
+    console.error("Unable to connect to the database:", error);
+    res.status(500).json({ message: "Ошибка сервера" });
   }
 });
 
-router.post("/esewa", authMiddleware, async (req, res) => {
-  try {
-    const { amount } = req.body;
-    const id = req.user.id;
-    const orderId = generateOrderId();
-    const transaction = await Transaction.create({
-      userId: id,
-      amount,
-      type: "deposit",
-      status: "pending",
-      orderId,
-    });
-    const user = await User.findByPk(id)
-    const deposit = await createDepositEsewa(orderId, amount * 1.01);
-    console.log(deposit)
-
-    if (deposit?.payment_url) {
-      return res.json({ url: deposit.payment_url });
-    }
-
-    return res.status(400).json({ success: false });
-  } catch (error) {
-    if (error instanceof AxiosError) {
-      console.error(error.response.data)
-    } else {
-      console.error("Unable to connect to the database:", error);
-    }
-    res.status(500).json({ message: "Server error" });
-  }
-});
+router.get('/notify', async (req, res) => {
+  console.log(req.query)
+  console.log('GET')
+  return res.status(200).send('OK')
+})
 
 router.post("/notify", async (req, res) => {
   try {
-    const { shop_transaction_id, status } = req.body;
     console.log(req.body);
-    if (!shop_transaction_id || !status) {
-      return res.status(200).send("OK");
-    }
-    const transaction = await Transaction.findOne({
-      where: { orderId: shop_transaction_id },
-    });
-    if (transaction && status === "succeeded") {
-      transaction.status = "completed";
-      if (transaction.type === "deposit") {
-        const user = await User.findByPk(transaction.userId);
-        user.balance += transaction.amount;
-        await user.save();
-      }
-      await transaction.save();
-    }
+    // if (!shop_transaction_id || !status) {
+    //   return res.status(200).send("OK");
+    // }
+    // const transaction = await Transaction.findOne({
+    //   where: { orderId: shop_transaction_id },
+    // });
+    // if (transaction && status === "succeeded") {
+    //   transaction.status = "completed";
+    //   if (transaction.type === "deposit") {
+    //     const user = await User.findByPk(transaction.userId);
+    //     user.balance += transaction.amount;
+    //     await user.save();
+    //   }
+    //   await transaction.save();
+    // }
 
     return res.status(200).send("OK");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
-    res.status(500).json({ message: "Server error" });
+    res.status(500).json({ message: "Ошибка сервера" });
   }
 });
 
-router.get('/khati/notify', async (req, res) => {
+router.post("/callback/anor", async (req, res) => {
   try {
-    const {purchase_order_id, status} = req.query
-    if (!purchase_order_id) {
-      return res.status(200).send("OK");
-    }
-    const transaction = await Transaction.findOne({
-      where: { orderId: purchase_order_id },
-    });
+    const body = req.body;
 
-    if (status === 'Completed') {
-      transaction.status = "completed";
-      if (transaction.type === "deposit") {
-        const user = await User.findByPk(transaction.userId);
-        user.balance += transaction.amount;
-        await user.save();
-      }
-      await transaction.save();
-    }
-    
-    return res.status(200).redirect(`${process.env.APP_URL}/deposit/confirmation?id=${transaction.id}`)
+    const { data } = await axios.post(
+      `${process.env.PAY_CRM_URL}/Remotes/callback-anor-deposit`,
+      body
+    );
+    console.log("PAY CRM RESPOSNSE:\n", data);
+    return res.status(200).send("OK");
   } catch (error) {
-    console.error("Unable to connect to the database:", error);
-    res.status(500).json({ message: "Server error" });
+    console.error(error);
+    res.status(500).json({ message: "Ошибка сервера" });
   }
-})
+});
 
 module.exports = router;
